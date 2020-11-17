23c9a68a742d86564d61a8763832769500d6176e
Pragadesh BS <71137576+PragadeshBS@users.noreply.github.com>
2020-11-17 13:08:09 +0530

    Update main.py
    Added features to track streak and total number of questions/problems solved till date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date
 from datetime import datetime
+from datetime import timedelta
 from os import system, name
 from random import randint
 import sys
@@ -42,7 +43,10 @@ def decrypt(message):
 
 daily_goal = 0
 today_date = date.today()
+yesterday_date = today_date - timedelta(days=1)
 today_weekday = day_name[today_date.weekday()]
+master_total = 0
+streak = streak_status = 0
 user_name = None
 remaining_questions_today = remaining_questions_yesterday = 0
 
@@ -58,11 +62,13 @@ def clear():
 
 
 def remaining_qs_unpacker():
-    global remaining_questions_today, remaining_questions_yesterday
+    global remaining_questions_today, remaining_questions_yesterday, streak_status, streak
     decrypt_file("data.txt")
     with open("data.txt", "r") as file:
         content = file.readlines()
     encrypt_file("data.txt")
+    streak = int(content[5][8:])
+    streak_status = int(content[6][15:])
     remaining_questions_list = content[2][14:].split(", ")
     remaining_questions_list = [int(i) for i in remaining_questions_list]
     remaining_questions_yesterday = remaining_questions_list[0]
@@ -80,10 +86,12 @@ def error_handler(code):
 
 
 def __init__():
-    global daily_goal, user_name
+    global daily_goal, user_name, master_total
     try:
         file_check = open("data.txt", "r")
+        file2_check = open("data.txt", "r")
         file_check.close()
+        file2_check.close()
     except FileNotFoundError:
         error_handler(0)
     try:
@@ -104,8 +112,10 @@ def __init__():
     if todo:
         remaining_qs_unpacker()
         daily_goal = int(content[1][12:])
+        master_total = int(content[4][14:])
         question_remaining_till_yesterday()
         user_name = content[3][11:len(content[3])-1]
+        streak_update()
         today_register()
         greet("old")
         save_changes(user_name, daily_goal)
@@ -125,13 +135,13 @@ def __init__():
     decrypt_file("data.txt")
     with open("data.txt", "w") as file_write:
         file_write.seek(0)
-        file_write.write(f"Not first launch\nDaily goal: {daily_goal}\nRemaining qs: 0, {daily_goal}\nuser_name: {user_name}")
+        file_write.write(f"Not first launch\nDaily goal: {daily_goal}\nRemaining qs: 0, {daily_goal}\nuser_name: {user_name}\nMaster total: 0\nStreak: 0\nStreak status: 0")
     encrypt_file("data.txt")
     today_register()
 
 
 def greet(status, recall=False):
-    global user_name, remaining_questions_yesterday, remaining_questions_today, daily_goal, today_weekday
+    global user_name, remaining_questions_yesterday, remaining_questions_today, daily_goal, today_weekday, master_total, streak
     current_time = datetime.now()
     if current_time.hour < 12:
         time_greet = "Good Morning"
@@ -144,6 +154,13 @@ def greet(status, recall=False):
             print(f"Hello {user_name}. Lets look at your pending questions...")
         else:
             print(f"{time_greet} {user_name}...Glad to see you back!")
+            if master_total > 1:
+                print(f"You have solved {master_total} problems in total, keep going")
+            elif master_total == 1:
+                print("You have solved 1 question in total, keep going")
+            else:
+                print("You have not solved any questions yet")
+            print(f"Your current streak is {streak}")
         if (remaining_questions_yesterday == 0) and (remaining_questions_today == 0):
             print("You currently have no questions to solve, that's great!")
             print(f"This is superb! Keep up the good work. Happy {today_weekday}!")
@@ -201,6 +218,23 @@ def reset():
     with open("data.txt", "w") as file:
         file.write("")
     encrypt_file("data.txt")
+
+
+def streak_update():
+    global streak_status, yesterday_date, streak
+    decrypt_file("data.txt")
+    with open("data.txt", "r+") as file:
+        contents = file.readlines()
+        last = contents[-1]
+        if last[1:11] == str(yesterday_date):
+            if streak_status == 1:
+                streak_status = 0
+            elif streak_status == 0:
+                streak = 0
+        elif last[1:11] != str(today_date):
+            streak_status = streak = 0
+    encrypt_file("data.txt")
+    return 0
 
 
 def today_register():
@@ -276,14 +310,14 @@ def question_remaining_till_yesterday():
 
 
 def save_changes(new_user_name, new_daily_goal):
-    global remaining_questions_today, remaining_questions_yesterday
+    global remaining_questions_today, remaining_questions_yesterday, master_total, streak, streak_status
     decrypt_file("data.txt")
     with open("data.txt", "r+") as file:
         contents = file.readlines()
-        initial_string = f"Not first launch\nDaily goal: {new_daily_goal}\nRemaining qs: {remaining_questions_yesterday}, {remaining_questions_today}\nuser_name: {new_user_name}\n"
+        initial_string = f"Not first launch\nDaily goal: {new_daily_goal}\nRemaining qs: {remaining_questions_yesterday}, {remaining_questions_today}\nuser_name: {new_user_name}\nMaster total: {master_total}\nStreak: {streak}\nStreak status: {streak_status}\n"
         file.seek(0)
         file.write(initial_string)
-        for content in contents[4:]:
+        for content in contents[7:]:
             if content[1:11] == str(today_date):
                 file.write(content)
         file.truncate()
@@ -291,7 +325,7 @@ def save_changes(new_user_name, new_daily_goal):
 
 
 def user_handler():
-    global daily_goal, user_name, remaining_questions_yesterday, remaining_questions_today
+    global daily_goal, user_name, remaining_questions_yesterday, remaining_questions_today, master_total, streak, streak_status
     while True:
         user_input = input("\nTry 'help' for a list of commands >>").lower().strip()
         if user_input == "help":
@@ -349,6 +383,7 @@ def user_handler():
                     break
             if update:
                 value = today_qs_register(num)
+                master_total += num
             else:
                 print("Your records were not updated, try again")
                 continue
@@ -356,6 +391,9 @@ def user_handler():
             if value == 1:
                 save_changes(user_name, daily_goal)
                 if remaining_questions_today == 0:
+                    if streak_status == 0:
+                        streak += 1
+                        streak_status = 1
                     print("Looks like you have achieved your daily goal...")
                     print("This is superb! Keep up the good work! See you tomorrow")
                 else:
